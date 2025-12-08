06d2350acd9383c46242efa4ff436e33b0fdf4b9
elenilsonguimaraes <guimaraes_elenilson@hotmail.com>
2025-12-08 17:12:59 -0300

    new commit

---
+++ cursojavaIntermediario/JavaIntermediario2025/src/com/elenilson/cursojavaIntermediario/aula53/Teste.java
@@ -0,0 +1,100 @@
+package com.elenilson.cursojavaIntermediario.aula53;
+
+public class Teste {
+	public static void main(String[] args) {
+		
+		usandoConstantes();
+		System.out.println();
+		usandoEnum();
+	}
+	
+	private static void usandoConstantes() {
+		
+		int segunda = DiaSemanaConstantes.SEGUNDA;
+		int terca = DiaSemanaConstantes.TERCA;
+		int quarta = DiaSemanaConstantes.QUARTA;
+		int quinta = DiaSemanaConstantes.QUINTA;
+		int sexta = DiaSemanaConstantes.SEXTA;
+		int sabado = DiaSemanaConstantes.SABADO;
+		int domingo = DiaSemanaConstantes.DOMINGO;
+		System.out.println("Teste utilizando constantes no Java");
+		imprimeDiaSemana(segunda);
+		imprimeDiaSemana(terca);
+		imprimeDiaSemana(quarta);
+		imprimeDiaSemana(quinta);
+		imprimeDiaSemana(sexta);
+		imprimeDiaSemana(sabado);
+		imprimeDiaSemana(domingo);
+	}
+	
+	private static void imprimeDiaSemana(int dia) {
+		switch(dia) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda-feira");
+			break;
+		case 3:
+			System.out.println("Terça-feira");
+			break;
+		case 4:
+			System.out.println("Quarta-feira");
+		case 5:
+			System.out.println("Quinta-feira");
+			break;
+		case 6: 
+			System.out.println("Sexta-feira");
+			break;
+		case 7:
+			System.out.println("Sábado");			
+		
+		}
+		
+	}
+	private static void imprimeDiaSemana(DiaSemana dia) {
+		switch(dia) {
+		case DOMINGO:
+			System.out.println("Domingo");
+			break;
+		case SEGUNDA:
+			System.out.println("Segunda-feira");
+			break;
+		case TERCA:
+			System.out.println("Terça-feira");
+			break;
+		case QUARTA:
+			System.out.println("Quarta-feira");
+		case QUINTA:
+			System.out.println("Quinta-feira");
+			break;
+		case SEXTA: 
+			System.out.println("Sexta-feira");
+			break;
+		case SABADO:
+			System.out.println("Sábado");			
+		
+		}
+		
+	}
+	private static void usandoEnum() {
+		
+		DiaSemana segunda = DiaSemana.SEGUNDA;
+		DiaSemana terca = DiaSemana.TERCA;
+		DiaSemana quarta = DiaSemana.QUARTA;
+		DiaSemana quinta = DiaSemana.QUINTA;
+		DiaSemana sexta = DiaSemana.SEXTA;
+		DiaSemana sabado = DiaSemana.SABADO;
+		DiaSemana domingo = DiaSemana.DOMINGO;
+		
+		System.out.println("Teste utilizando enum no java:");
+		imprimeDiaSemana(segunda);
+		imprimeDiaSemana(terca);
+		imprimeDiaSemana(quarta);
+		imprimeDiaSemana(quinta);
+		imprimeDiaSemana(sexta);
+		imprimeDiaSemana(sabado);
+		imprimeDiaSemana(domingo);
+	}
+
+}
