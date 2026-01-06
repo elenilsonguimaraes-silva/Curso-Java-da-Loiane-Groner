fc6e21d469c7255ef9a037c133688bffe2ba13a1
elenilsonguimaraes <guimaraes_elenilson@hotmail.com>
2026-01-06 12:48:10 -0300

    update e upload de arquivos

---
+++ JavaBasico2025/src/com/elenilson/cursojavaintermediario/aula72/Teste.java
@@ -0,0 +1,21 @@
+package com.elenilson.cursojavaintermediario.aula72;
+
+public class Teste {
+	public static void main(String[] args) {
+		
+		TiqueTaque tt = new TiqueTaque();
+		ThreadTiqueTaque tique = new ThreadTiqueTaque("Tique", tt);
+		ThreadTiqueTaque taque = new ThreadTiqueTaque("Taque", tt);
+		
+		try {
+			tique.t.join();
+			taque.t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
---
+++ JavaBasico2025/src/com/elenilson/cursojavaintermediario/aula72/TiqueTaque.java
@@ -0,0 +1,55 @@
+package com.elenilson.cursojavaintermediario.aula72;
+
+public class TiqueTaque {
+	
+	boolean tique;
+	
+	 synchronized void tique(boolean estaExecutando) {
+		
+		 if(!estaExecutando) {
+			 tique = true;
+			 notify();
+			 return;
+		 }
+		 
+		 System.out.print("Tique ");
+		 
+		 tique = true;
+		 
+		 notify();
+		 
+		 try {
+			 while(tique) {
+				 wait();
+				 
+			 }
+		 }catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	 
+	 synchronized void taque(boolean estaExecutando) {
+			
+		 if(!estaExecutando) {
+			 tique = false;
+			 notify();
+			 return;
+		 }
+		 
+		 System.out.println("Taque ");
+		 
+		 tique = false;
+		 
+		 notify();
+		 
+		 try {
+			 while(!tique) {
+				 wait();
+				 
+			 }
+		 }catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
