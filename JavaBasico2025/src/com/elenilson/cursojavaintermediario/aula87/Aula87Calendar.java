61f5409557b7d81ab2a95e75cf39f23a4ecddada
elenilsonguimaraes <guimaraes_elenilson@hotmail.com>
2026-01-15 19:20:04 -0300

    upload e update de arquivos

---
+++ JavaBasico2025/src/com/elenilson/cursojavaintermediario/aula87/Aula87Calendar.java
@@ -0,0 +1,37 @@
+package com.elenilson.cursojavaintermediario.aula87;
+
+import java.util.Calendar;
+
+public class Aula87Calendar {
+
+	public static void main(String[] args) {
+		
+		Calendar hoje = Calendar.getInstance(); //singleton
+		
+		int ano = hoje.get(Calendar.YEAR);
+		int mes = hoje.get(Calendar.MONTH);
+		int dia = hoje.get(Calendar.DAY_OF_MONTH);
+		int hora = hoje.get(Calendar.HOUR_OF_DAY);
+		int minutos  = hoje.get(Calendar.MINUTE);
+		int segundos = hoje.get(Calendar.SECOND);
+		
+		System.out.println(ano);
+		System.out.println(mes);
+		System.out.println(dia);
+		System.out.println(hora);
+		System.out.println(minutos);
+		System.out.println(segundos);
+		
+		System.out.printf("Hoje é: %02d/%02d/%d %02d:%02d:%02d\n",dia, (mes + 1),  ano, hora, minutos, segundos);		
+		
+		
+		hoje.add(Calendar.DAY_OF_MONTH, -1);
+		
+		hoje.add(Calendar.DAY_OF_MONTH, 6);
+		
+		System.out.println();
+		
+		System.out.println(hoje.get(Calendar.DAY_OF_MONTH));
+	}
+
+}
