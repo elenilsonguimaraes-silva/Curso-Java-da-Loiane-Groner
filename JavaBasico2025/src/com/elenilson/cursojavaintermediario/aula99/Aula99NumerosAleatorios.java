61df9c2689c20e303962215103e0dec8af1cd5e1
elenilsonguimaraes <guimaraes_elenilson@hotmail.com>
2026-01-20 23:07:59 -0300

    update de arquivos

---
+++ JavaBasico2025/src/com/elenilson/cursojavaintermediario/aula99/Aula99NumerosAleatorios.java
@@ -0,0 +1,19 @@
+package com.elenilson.cursojavaintermediario.aula99;
+
+import java.util.Random;
+
+public class Aula99NumerosAleatorios {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.floor(Math.random() * 100));
+		
+		Random aleatorio = new Random();
+		
+		System.out.println(aleatorio.nextInt());
+		
+		System.out.println(aleatorio.nextInt(5 + 1));
+
+	}
+
+}
