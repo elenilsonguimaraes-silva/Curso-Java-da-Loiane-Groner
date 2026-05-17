1d5f69530a58bfa310dd7b28c694f4b8dffd0146
Elenilson G. Silva <videos.do.le@gmail.com>
2026-05-17 09:11:39 -0300

    update
    update

--- JavaBasico2025/src/com/elenilson/estruturasDeDadosJava/pilha/labs/Exercicio6_Correcao_Loiane.java
+++ JavaBasico2025/src/com/elenilson/estruturasDeDadosJava/pilha/labs/Exercicio6_Correcao_Loiane.java
@@ -1,10 +1,55 @@
 package com.elenilson.estruturasDeDadosJava.pilha.labs;
 
+import com.elenilson.estruturasDeDadosJava.pilha.Pilha;
+
 public class Exercicio6_Correcao_Loiane {
 
 	public static void main(String[] args) {
 		
+		imprimeResultado("A + B");
+		imprimeResultado("A + B + (C - D)");
+		imprimeResultado("{[()]}[](){}");
+		imprimeResultado("{[(]}[](){}");
+		imprimeResultado("A + B +c - D)");
+		imprimeResultado("(((");
+		
 
+	}
+	public static void imprimeResultado(String expressao) {
+		System.out.println(expressao + " está  balanceado? " +
+				verificaSimbolosBalanceados(expressao));
+	}
+	
+	final static String ABRE = "([{";
+	final static String FECHA = ")]}";
+	public static boolean verificaSimbolosBalanceados(String expressao) {
+		
+		
+		Pilha<Character> pilha = new Pilha<>();		
+		int index = 0;
+		char simbolo, topo;
+		
+		while(index < expressao.length()) {
+			simbolo = expressao.charAt(index);
+			
+			if(ABRE.indexOf(simbolo) > -1) {
+				pilha.empilha(simbolo);
+			}else if(FECHA.indexOf(simbolo) > -1) {
+				
+				if(pilha.estaVazia()) {
+					return false;
+				}else {
+					topo = pilha.desempilha();
+					if(ABRE.indexOf(topo) != FECHA.indexOf(simbolo)) {
+						return false;
+					}
+				}
+			}			
+			
+			index++;
+		}
+		
+		return pilha.estaVazia();
 	}
 
 }
